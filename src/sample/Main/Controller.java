9fff581e06aaf4f571c66cb7373a0253d4aad274
fuentesmarlon <fue15240@uvg.edu.gt>
2018-03-14 17:11:13 -0600

    Comenzamos
    GUI ya hecha

---
+++ src/sample/Main/Controller.java
@@ -0,0 +1,4 @@
+package sample.Main;
+
+public class Controller {
+}
---
+++ src/sample/Main/Main.java
@@ -0,0 +1,23 @@
+package sample.Main;
+
+import com.jfoenix.controls.JFXTabPane;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("SQLPane.fxml"));
+        primaryStage.setTitle("MiSICUEL");
+        primaryStage.setScene(new Scene(root, 750, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
